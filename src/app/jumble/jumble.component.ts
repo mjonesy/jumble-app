929b17134c5400a68575a7996f7e70a03225aa55
Matt Jones <jonesy0007@gmail.com>
2022-09-13 20:12:09 -0500

    remove difficulty settings, refactor setScrambledWord()

--- src/app/jumble/jumble.component.ts
+++ src/app/jumble/jumble.component.ts
@@ -13,7 +13,6 @@ export class JumbleComponent implements OnInit {
   isWordCorrect = false;
   showGuessAgain = false;
   guessedWords: string[] = [];
-  difficulty = 0;
 
   constructor() {
   }
@@ -22,65 +21,39 @@ export class JumbleComponent implements OnInit {
     this.setScrambledWord();
   }
 
-  setDifficulty(difficulty: number) {
-    switch (difficulty) {
-      case 3:
-        this.difficulty = 3;
-        this.setScrambledWord();
-        break;
-      case 5:
-        this.difficulty = 5;
-        this.setScrambledWord();
-        break;
-      case 8:
-        this.difficulty = 8;
-        this.setScrambledWord();
-        break;
-      case 10:
-        this.difficulty = 10;
-        this.setScrambledWord();
-        break;
-      default:
-        break;
-    }
-  }
-
   setScrambledWord() {
-    let wordToShuffle = "";
+    this.unscrambledWord = this.generateWord();
 
-    // if (this.difficulty != 0) {
-    //   wordToShuffle = randomWords.words({maxLength: this.difficulty})[0];
-    // } else {
-      wordToShuffle = randomWords.default(1)[0];
-    // }
-
-    this.unscrambledWord = wordToShuffle;
-    console.log('wordToShuffle = ', wordToShuffle);
-
-    this.scrambleWord(wordToShuffle);
-    if (this.scrambledWord == wordToShuffle) {
-      this.scrambleWord(wordToShuffle);
-    }
-
-    console.log('shuffledWord = ', this.scrambledWord);
+    this.scrambledWord = this.scrambleCheckForDupes(this.unscrambledWord);
 
     return this.scrambledWord;
   }
 
-  scrambleWord(wordToShuffle: string) {
-    console.log('scrambling ', wordToShuffle);
+  generateWord() {
+    return randomWords.default(1)[0];
+  }
 
-    let splitShuffledWord = wordToShuffle.split('');
+  scrambleCheckForDupes(unscrambled: string) {
+    let scrambledWord = '';
 
-    splitShuffledWord.sort(function () {
+    do {
+      scrambledWord = this.scrambleWord(unscrambled);
+    } while (scrambledWord == unscrambled);
+
+    return scrambledWord;
+  }
+
+  scrambleWord(wordToScramble: string) {
+    let scrambledWordLetters = wordToScramble.split('');
+
+    scrambledWordLetters.sort(function () {
       return 0.5 - Math.random();
     });
-    this.scrambledWord = splitShuffledWord.join('');
+
+    return scrambledWordLetters.join('');
   }
 
   checkInputWord(event: any) {
-    // console.log('wordsList = ', randomWords.default(1));
-
     this.guessedWord = event.target.value;
     console.log(this.guessedWord);
     const correctGuess = this.guessedWord == this.unscrambledWord;
