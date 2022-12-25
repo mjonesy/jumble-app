8533262316b95c2f7f22ddc19cdbb9db0d589639
Matt Jones <jonesy0007@gmail.com>
2022-12-25 17:59:06 -0500

    add server.js, modify package.json

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+// Serve static files
+app.use(express.static(__dirname + '/dist/jumble-app'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/jumble-app/index.html'));
+});
+// default Heroku port
+app.listen(process.env.PORT || 5000);
